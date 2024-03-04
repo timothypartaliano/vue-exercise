bbd8dc0b342b7a27f7c874ea1ecfe6dd265e8a94
timothypartaliano <timothypartaliano@gmail.com>
2024-03-04 17:21:04 +0700

    add fetchUsers component

---
+++ src/components/Users.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { useUserStore } from '@/stores/user';
+const store = useUserStore()
+
+store.fetchUsers()
+</script>
+<template>
+    <div class="user">
+        <ul v-for="user in store.users">
+            <li>{{ user.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<style>
+.user {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+</style>
